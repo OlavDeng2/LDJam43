5fa6d0c0c94d380733c748254358536b592a8b74
OlavDeng2 <olavkruithof@gmail.com>
2018-12-01 12:17:38 +0900

    Boop

--- Assets/LDJam43/Scripts/Menu/MainMenuManager.cs
+++ Assets/LDJam43/Scripts/Menu/MainMenuManager.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 
+    private Panel[] menuPanels;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        menuPanels = GetComponentsInChildren<Panel>(true);
 	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
